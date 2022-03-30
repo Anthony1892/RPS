2c53696c2ded0985275347af657eb964fcd1623d
Anthony Mkandawire <anthonymkandawire@gmail.com>
2022-03-30 15:28:59 +0200

    edit index.html and script/

--- script/script.js
+++ script/script.js
@@ -1,47 +1,63 @@
-prompt("Make a choice:")
+const userChoiceDisplay = document.createElement('h2');
+const computerChoicesDisplay = document.createElement('h2');
+const resultDisplay = document.createElement('h2');
+const gameGrid = document.getElementById('game');
+gameGrid.append(userChoiceDisplay, computerChoicesDisplay, resultDisplay);
 
-let userSelection = userPlay();
-function userPlay(){
-    let random = ["Rock", "Paper", "Scissors"];
-    return random[Math.floor(Math.random() * 3)];
+const choices = ['Rock', 'Paper', 'Scissors'];
+let userChoice
+let computerChoice
+
+const handleClick = (e) => {
+  userChoice = e.target.id;
+  userChoiceDisplay.innerHTML = 'user choice: ' + userChoice;
+  generateComputerChoice();
+  getResult()
+};
+
+const generateComputerChoice = () => {
+  const randomChoice = choices[Math.floor(Math.random() * choices.length)];
+  computerChoice = randomChoice
+  computerChoicesDisplay.innerHTML = random = randomHTML = 'computer choice: ' + computerChoice;
 }
 
-let computerSelection = computerPlay();
-
-function computerPlay() {
-    let random = ["Rock", "Paper", "Scissors"];
-    return random[Math.floor(Math.random() * 3)];
+for (let i = 0; i < choices.length; i++) {
+  const button = document.createElement('button');
+  button.id = choices[i];
+  button.innerHTML = choices[i];
+  button.addEventListener('click', handleClick);
+  gameGrid.appendChild(button);
 }
 
-function playRound(playerSelection, computerSelection)
-{
-    if (playerSelection === "Rock") {
-        if (computerSelection === "Rock")
-        {
-            return "Draw.";
-        } else if (computerSelection === "Paper"){
-            return "Computer wins!.";
-        } else {
-            return "User wins.";
-        }
-    }
-    else if (playerSelection === "Paper") {
-        if (computerSelection === "Rock")
-        {
-            return "User wins!.";
-        } else if (computerSelection === "Paper") {
-            return "Draw.";
-        } else {
-            return "Computer wins!.";
-        }
-    } else {
-        if (computerSelection === "Rock") {
-            return "Computer wins!.";
-        } else if (computerSelection === "Paper") {
-            return "User wins!.";
-        } else {
-            return "Draw.";
-        }
-    } 
+const getResult = () => {
+  switch (userChoice + computerChoice) {
+    case 'ScissorsPaper':
+    case 'RockScissors':
+    case 'PaperRock':
+      resultDisplay.innerHTML = "You Win!"
+      break
+    case 'PaperScissors':
+    case 'ScissorsRock':
+    case 'RockPaper':
+      resultDisplay.innerHTML = "You lost"
+      break
+    case 'PaperPaper':
+    case 'RockRock':
+    case 'ScissorsScissors':
+      resultDisplay.innerHTML = "It's a Tie!"
+      break
+  }
 }
-console.log(playerRound(userPlay, computerSelection));
+  
+/*  let userScore = parseInt(0);
+  let computerScore = parseInt(0);
+  let win = "You win"
+  let lose = "You lose"
+  let tie = "It is a tie"
+  
+  for(var i=0;i<5;i++){
+    const computerSelection = computerPlay()
+    console.log(playRound(playerSelection, computerSelection))
+    console.log("your score = " + userScore);
+    console.log("Computer's score = " + computerScore);
+  }*/
